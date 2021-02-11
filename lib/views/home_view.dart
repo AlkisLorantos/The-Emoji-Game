da21a5e1914e4fe967846ec797691c6ef632e64a
Alkis <alkis.lorantos@icloud.com>
2021-02-11 13:34:33 +0200

    another one

---
+++ lib/views/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          
+        )
+    );
+  }
+}
