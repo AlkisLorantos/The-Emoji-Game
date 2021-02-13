3dbcb0905e38bb6849c0bd76f832b6c0b9bf16cb
Alkis <alkis.lorantos@icloud.com>
2021-02-13 14:01:46 +0200

    Removed Files

--- lib/views/auth/sign_up_view.dart
+++ lib/views/auth/sign_up_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:the_emoji_game/size_config.dart';
 
 class SignUpView extends StatefulWidget {
   @override
@@ -9,47 +8,8 @@ class SignUpView extends StatefulWidget {
 class _SignUpViewState extends State<SignUpView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Text("Register Account", 
-          style: TextStyle(
-            fontSize: getProportionateScreenWidth(28),
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-            height: 1.5, 
-            ),
-          ),
-          Text(
-            "Complete your details or continue \nwith social media",
-            textAlign: TextAlign.center,
-          ),
-          SignUpForm(),
-        ],
-      ),
-    );
-  }
-}
-
-class SignUpForm extends StatefulWidget {
-  @override
-  _SignUpFormState createState() => _SignUpFormState();
-}
-
-class _SignUpFormState extends State<SignUpForm> {
-  
-  final _formKey = GlobalKey<FormState>();
-  String email;
-  String password;
-  String confirm_password;
-  final List<String> errors = [];
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
       
     );
   }
 }
-
-
